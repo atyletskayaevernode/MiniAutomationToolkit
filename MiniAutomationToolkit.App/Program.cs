6bca679cba63f4c14031278714311e2aaa4d813a
atyletskayaevernode <a.tyletskaya@evernode.tech>
2026-08-01 14:54:06 +0200

    Задание 3. «Поиск в хаосе»

--- MiniAutomationToolkit.App/Program.cs
+++ MiniAutomationToolkit.App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using MiniAutomationToolkit.Core.Models;
 using MiniAutomationToolkit.Core.Services;
+using System.Diagnostics;
 using static MiniAutomationToolkit.Core.Models.ClientType;
+using MiniAutomationToolkit.Core.Helpers;
 
 Console.WriteLine("MiniAutomationToolkit started");
 Console.WriteLine("===");
@@ -24,3 +26,68 @@ foreach (var (client, amount) in taskTwoExamples)
     Console.WriteLine($"Client: {client}, amount: {amount}, discount: {discount}");
 }
 Console.WriteLine("===");
+
+Console.WriteLine("Task 3 test (FileSearcher)");
+
+var fileNames = new List<string> //список с png файлами
+{
+    "screen_001.jpeg",
+    "screen_002.jpg",
+    "error_2024.log",
+    "screen_003.png",
+    "SCREEN_004.PNG",
+    "debug.txt",
+    "screenshot_final.PnG",
+    "image.bmp",
+    "photo.gif",
+    "archive.zip",
+    "document.pdf",
+    "presentation.pptx",
+    "spreadsheet.xlsx",
+    "notes.txt",
+    "script.js",
+    "style.css",
+    "index.html",
+    "README.md",
+    "config.json",
+    "data.xml"
+};
+
+string firstScreenshot = FileSearcher.FindFirstScreenshot(fileNames); //поиск по списку 1
+Console.WriteLine($"First .png file: {firstScreenshot}");
+
+var fileNamesWithoutScreenshots = new List<string> //список без png
+{
+    "001.jpeg",
+    "002.jpg",
+    "error_2024.log",
+    "003.jpg",
+    "004.jpg",
+    "debug.txt",
+    "screenshot_final",
+    "image.bmp",
+    "photo.gif",
+    "archive.zip",
+    "document.pdf",
+    "presentation.pptx",
+    "spreadsheet.xlsx",
+    "notes.txt",
+    "script.js",
+    "style.css",
+    "index.html",
+    "README.md",
+    "config.json",
+    "data.xml"
+};
+
+try //поиск по списку 2, плюс обработка эксепшена
+{
+    FileSearcher.FindFirstScreenshot(fileNamesWithoutScreenshots);
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine("===");
+
+
---
+++ MiniAutomationToolkit.Core/Helpers/FileSearcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MiniAutomationToolkit.Core.Helpers;
+
+public static class FileSearcher
+{
+    public static string FindFirstScreenshot(List<string> fileNames) // метод поиска первого .png файла в списке файлов
+    {
+        var screenshots = fileNames.Where(name =>
+            name.EndsWith(".png", StringComparison.OrdinalIgnoreCase));
+
+        if (!screenshots.Any()) // экспешн на случай, если такого файла нет
+        {
+            throw new FileNotFoundException("No screenshots found in the provided list.");
+        }
+
+        return screenshots.FirstOrDefault()!;
+    }
+}
+
